42d6e8130456a09d3fc2d9beed0267a76d66041f
Oliver Harrison <olivermharrison@gmail.com>
2017-10-10 20:11:13 +0100

    [+] Initial commit: basic project structure

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+camera.position.z = 50;
+
+let controls;
+controls = new THREE.OrbitControls( camera );
+
+var renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+
+
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+
+var animate = function () {
+  requestAnimationFrame( animate );
+
+  renderer.render(scene, camera);
+};
+
+animate();
