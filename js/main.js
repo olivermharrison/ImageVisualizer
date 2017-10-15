3731bb593b8b511e03e9edf9513ba8aea06c922e
Oliver Harrison <olivermharrison@gmail.com>
2017-10-15 21:38:22 +0100

    [+] Support for downloading the output image

--- js/main.js
+++ js/main.js
@@ -58,7 +58,6 @@ var animate = function () {
   //camera.position.y = radius * Math.sin( THREE.Math.degToRad( theta ) );
   camera.position.z = radius * Math.cos( THREE.Math.degToRad( theta ) );
   camera.lookAt( scene.position );
-
   graph.update();
 
   renderer.render(scene, camera);
@@ -66,3 +65,11 @@ var animate = function () {
 
 animate();
 
+function dlCanvas() {
+  var dt = outputCanvas.toDataURL('image/png');
+  dt = dt.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
+  dt = dt.replace(/^data:application\/octet-stream/, 'data:application/octet-stream;headers=Content-Disposition%3A%20attachment%3B%20filename=Canvas.png');
+
+  this.href = dt;
+};
+document.getElementById("dl").addEventListener('click', dlCanvas, false);
